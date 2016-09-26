d6e85d8769919ab8ea17e39b0ab843f3d833bf59
JJ Crossman <jj.crossman@gmail.com>
2016-09-26 14:13:06 -0500

    blarg

--- server.js
+++ server.js
@@ -1,32 +1,39 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var cors = require('cors');
+// Server REQUIRES
+const express = require( 'express' );
+const { json } = require( 'body-parser' );
+const cors = require( 'cors' );
+const mongoDb = require( 'mongojs' );
+const app = express();
 
-var app = express();
-app.use(bodyParser.json());
+// Server Middleware
+app.use( json() );
 
-var port = 3000;
+const port = 3000;
 
+
+// CRUD / REST
 app.post('/api/sighting', function(req, res) {
   console.log('POST sighting');
   res.end();
 });
- 
+
 app.get('/api/sighting', function(req, res) {
   console.log('GET sighting');
   res.end();
 });
- 
+
 app.delete('/api/sighting', function(req, res) {
   console.log('DELETE sighting');
   res.end();
 });
- 
+
 app.put('/api/sighting', function(req, res) {
   console.log('PUT sighting');
   res.end();
 });
 
+
+// LISTEN 
 app.listen(port, function() {
-  console.log("Started server on port", port);
+  console.log( "Birds DB started listening on port: " + port );
 });
